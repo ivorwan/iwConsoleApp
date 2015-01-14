eca213df4a46c4710fd03671c390d7ce9f4edb8a
ivorwan <riogangsta@gmail.com>
2015-01-14 10:42:29 -0800

    algo practicing

--- iwConsoleApp/Program.cs
+++ iwConsoleApp/Program.cs
@@ -13,11 +13,23 @@ namespace iwConsoleApp
     {
         static void Main(string[] args)
         {
+
             //string test = null;
 
+            ArraysAndStrings q1 = new ArraysAndStrings();
+
+            foreach (var method in q1.GetType().GetMethods())
+            {
+                method.Invoke(q1, null);
+            }
+
+            /*
             LINQExamples ex = new LINQExamples();
             List<dynamic> result = ex.CountCharsInStringDynamic("iwdwds");
+             
             Console.WriteLine(result[0].Key);
+             */
+
             Console.ReadKey();
 
         }

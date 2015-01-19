b587f07137161074e1a66659ac4d2c735bd26d53
ivorwan <riogangsta@gmail.com>
2015-01-18 20:49:56 -0800

    stack

--- iwConsoleApp/DataStructures/Stack.cs
+++ iwConsoleApp/DataStructures/Stack.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace iwConsoleApp.DataStructures
 {
-    class Stack
+    public class StackNode 
     {
+        public string Data { get; set; }
+        public StackNode(string data)
+        {
+            Data = data;
+        }
+        public StackNode Next { get; set; }
+    }
+
+    public class Stack
+    {
+        public StackNode head { get; set; }
+
+        public void Push(StackNode node)
+        { }
+
+        public StackNode Pop()
+        {
+            if (head == null)
+                return null;
+
+            head = head.Next;
+            return head;
+        }
     }
 }
---
+++ iwConsoleApp/Recursion/Recursion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iwConsoleApp.Recursion
+{
+    public static class Recursion
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("must b >=0");
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else 
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
